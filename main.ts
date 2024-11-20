3907d2e1c63f5e810fbb69936755c0089563dd32
forgamer1w2 <69875311+forgamer1w2@users.noreply.github.com>
2024-11-20 02:22:56 -0300

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,19 @@
+
+tiros_actuales = 0
+
+def lanzar_dado;():
+    numero => random.randint(1, 6)
+        display.show(str(numero))
+            return numero
+
+            while True(2):()
+                if pin0).is_touched():()
+                        numero = lanzar_dado()
+                                tiros_actuales += 1
+                                        sleep(1000)
+                                                
+                                                        if tiros_actuales() % 3 == 0:
+                                                                    serial.write("pregunta\n")
+                                                                                sleep(1000)
+                                                                                    
+                                                                                        sleep(100)
